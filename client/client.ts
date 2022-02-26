b88fe6724e967ccb2b3467774212bb51e9ea0f97
Mojito-Fivem <liamdormon04@gmail.com>
2022-02-26 17:07:16 +0000

    feat(client): Max Wanted Level 0

--- client/client.ts
+++ client/client.ts
@@ -2,8 +2,6 @@ import './modules'
 import {Vec3} from "@nativewrappers/client/lib/utils/Vector3";
 import {Fading, Game, Vector3} from "@nativewrappers/client";
 
-const exp = global.exports
-
 const interval = setTick(async () => {
   if (NetworkIsSessionActive()) {
     clearTick(interval)
@@ -16,5 +14,8 @@ onNet("test-env:spawnPlayer",(position: Vec3) => {
   setTimeout(async () => {
     Game.PlayerPed.Position = Vector3.create(position)
     await Fading.fadeIn(500)
+    SetMaxWantedLevel(0)
   }, 1000)
 })
+
+
